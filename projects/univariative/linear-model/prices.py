effa133e1b5801859536af8640c55d88937935c2
nemesysolano@gmail.com <nemesysolano@gmail.com>
2018-03-26 23:29:25 -0400

    Finished LinearRegression use case with prices data

--- projects/univariative/linear-model/prices.py
+++ projects/univariative/linear-model/prices.py
@@ -2,10 +2,25 @@ import numpy
 import sys
 import pandas
 from sklearn.linear_model import LinearRegression
+from sklearn import cross_validation
 
 if __name__ == "__main__":
     sales_file = sys.argv[1]
     sales_data = pandas.read_csv(sales_file, parse_dates=[0], infer_datetime_format = True)
-    X = sales_data.drop('Q (Sales)', axis = 1)
-    Y = sales_data['Q (Sales)']
-    X_train, X_test, Y_train, Y_test = sklearn.cross_validation.train_test_split(X, Y, test_size = 0.33, random_state = 5)
+    X = sales_data.drop('Sales', axis = 1)    
+    Y = numpy.array(tuple(map( lambda x: float(x), sales_data['Sales'])))
+
+    X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(X, Y, test_size = 0.33, random_state = 5)
+
+    linear_regression = LinearRegression()
+    linear_regression.fit(X_train, Y_train)
+    pred_train = linear_regression.predict(X_train)
+    pred_test = linear_regression.predict(X_test)
+
+    rmse_train = numpy.sqrt(numpy.mean((Y_train - linear_regression.predict(X_train)) ** 2))
+    rmse_test = numpy.sqrt(numpy.mean((Y_test - linear_regression.predict(X_test)) ** 2))
+    rmse_ratio = numpy.abs(rmse_train - rmse_test) / numpy.max((rmse_train, rmse_test))
+
+    print("Fit a model X_train, and calculate RMSE with Y_train: %8.2f" % rmse_train)
+    print("Fit a model X_train, and calculate RMSE with X_test, Y_test: %8.2f" % rmse_test)
+    print("RMSE = %5.2f" % rmse_ratio )
