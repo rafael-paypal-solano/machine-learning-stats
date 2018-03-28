09c340b1b90e5d9f4885fdbb561f0ac07cbd1ae2
Rafael Solano <rsolano@castlebreck.com>
2018-03-28 16:05:05 -0400

    Quick backup

--- lib/numeric/pearson.py
+++ lib/numeric/pearson.py
@@ -5,12 +5,18 @@ import pathos.multiprocessing as multiprocessing
 MIN_TAYLOR_TIME_SEQ_ORDER = 4
 MAX_TAYLOR_TIME_SEQ_ORDER = 8
 
-def taylor_time_seq(y, order = MIN_TAYLOR_TIME_SEQ_ORDER, pool = None):
-    """
-        Args:
-            y (sequence of numbers): numpy.ndarray, list, set, tuple
-            order (int): MIN_TAYLOR_TIME_SEQ_ORDER <= order <=  MIN_TAYLOR_TIME_SEQ_ORDER
-            
-        Returns:
+class TaylorTimeModel    
+    def __init__(y, order = MIN_TAYLOR_TIME_SEQ_ORDER, pool = None):
+        """
+            Args:
+                y (sequence of numbers): numpy.ndarray, list, set, tuple
+                order (int): MIN_TAYLOR_TIME_SEQ_ORDER <= order <=  MIN_TAYLOR_TIME_SEQ_ORDER
+                
+            Returns:
 
-    """
+        """
+
+        pass
+
+    def __call__(self, x):
+        pass
