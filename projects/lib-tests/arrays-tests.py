4f2f4f746b0bed87bfda9cbe72e23054ba2bff8a
nemesysolano@gmail.com <nemesysolano@gmail.com>
2018-04-17 23:14:57 -0400

    partial commit.

--- projects/lib-tests/arrays-tests.py
+++ projects/lib-tests/arrays-tests.py
@@ -1,21 +1,42 @@
 from environment import Arrays
+import pycuda
 import numpy
+from pycuda.gpuarray import GPUArray
+import pycuda.gpuarray 
+import pycuda
+import skcuda.misc
+import skcuda.linalg
+from numeric import GPUReductor
+
+
 
 if __name__ == "__main__":
-    a = Arrays.create(
+    a = numpy.array(
         (
-            (1,   2.0),
-            (3.0, 1.0)
+            (1,   3.0, 5),
+            (7.0, 11, 9),
+            (13, 17.0, 19),
         )
     )
-    b = numpy.array(((1,2.0),(3.0, 1)))
+    b = Arrays.to_array(a)
+
+    print(type(a), type(b))
+
     print(Arrays.sum(a))
     print(Arrays.sum(b))
 
-    A = Arrays.sum(a,0)
-    B = Arrays.sum(b,0)
+    c = Arrays.sum(a,0)
+    d = Arrays.sum(b,0)
+    f = Arrays.sum(a,1)
+    g = Arrays.sum(b,1)
 
-    print(type(A))
-
-    print(Arrays.sum(a,1))
-    print(Arrays.sum(b,1))        
+    print(c)
+    print(d)        
+    print(f)
+    print(g)        
+    
+    print(GPUReductor.correction_for_mean_block(b))    
+    print(GPUReductor.sum_of_squares_block(b))
+    print(GPUReductor.sum_of_squares_total_block(b))
+    print(GPUReductor.mean_block(b))
+    print(GPUReductor.standard_squared_error_block(b))
