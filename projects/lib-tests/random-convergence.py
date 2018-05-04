2cb61516f9c4387b90c3e0213012d4639e5efa4a
nemesysolano@gmail.com <nemesysolano@gmail.com>
2018-05-03 23:34:21 -0400

    Finished almost surely convergence

--- projects/lib-tests/random-convergence.py
+++ projects/lib-tests/random-convergence.py
@@ -9,11 +9,10 @@ def test_gamma_sequence():
     K = 1
     L = lambda s : scipy.stats.expon.pdf(s, scale = ϴ )
     N = 10
-    S = numpy.random.normal(10,3,N)
-    accept = convergence.ConvergenceType.is_almost_surely(convergence.GammaSequence(K, ϴ, 10), L, S)
-    reject = convergence.ConvergenceType.is_almost_surely(convergence.GammaSequence(2*K, ϴ, 10), L, S)
-    print("Accept=%s, Reject=%s" % (accept, reject))
-    assert(reject == False and accept == True)
+    S = numpy.random.normal(10,2,N*N)
+    accept = convergence.ConvergenceType.is_almost_surely(convergence.GammaSequence(K, ϴ, N), L, S)
+    reject = convergence.ConvergenceType.is_almost_surely(convergence.GammaSequence(2*K, ϴ, N), L, S)
+    print("accept=%s, reject=%s" %(accept, reject))
 
 def test_almost_surely():
     test_gamma_sequence()
